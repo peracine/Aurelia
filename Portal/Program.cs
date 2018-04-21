2febbcf0591174d6ef2a62a9fbf85e29b62d00a6
Pierre <peracine@yahoo.com>
2018-04-21 14:21:33 +0200

    NLog implementation

--- Portal/Program.cs
+++ Portal/Program.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
 
 namespace Portal
 {
@@ -14,12 +10,33 @@ namespace Portal
     {
         public static void Main(string[] args)
         {
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("NLog initialized.");
+                BuildWebHost(args).Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "NLog failed to start.");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog()
                 .Build();
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Aurelia
+# Aurelia & .Net Core
 
 ## Setting up your machine with VS 2017
  * .NET CLI from https://www.microsoft.com/net/core
@@ -8,5 +8,9 @@
  -> Aurelia doesn't come as a standard template
 ```
  dotnet new --install "Microsoft.AspNetCore.SpaTemplates::*"
- dotnet new aurelia -o "D:\GIT\Aurelia" --force
+ dotnet new aurelia -o "Path/To/The/Project" --force
 ```
+
+## Features
+* Nlog (https://github.com/NLog/NLog.Web/wiki/Getting-started-with-ASP.NET-Core-2)
+	Bug concerning internalLogFile https://github.com/NLog/NLog.Web/issues/201
